8761ae30de618f24ae4e9fd28418ee6234c3431c
VartanovMaks <vartanov2009@gmail.com>
2021-08-09 15:50:24 +0300

    Access-Control-Allow-Origin solved

--- base/controllers/game.controller.js
+++ base/controllers/game.controller.js
@@ -3,6 +3,9 @@ const { Game } = require('../dataBase');
 module.exports = {
   getAllGames: async (req, res, next) => {
     try {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       const games = await Game.find({});
       res.json(games);
     } catch (e) {
